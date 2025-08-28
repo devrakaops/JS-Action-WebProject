10ad303e809d7e531112dd610f2e475289488e99
devrakaops <dollyjangid40@gmail.com>
2025-08-28 08:18:16 +0000

    Added files again

--- app.js
+++
@@ -1,5 +0,0 @@
-function greet() {
-  document.getElementById("output").innerText = "Hello from JavaScript!";
-}
-
-module.exports = { greet };  // For testing with Jest
--- test_app.js
+++
@@ -1,5 +0,0 @@
-const { greet } = require("./app");
-
-test("greet function exists", () => {
-  expect(typeof greet).toBe("function");
-});
